495cd0b2a9a4c1e3b353cf08bd9200da1fc55ae8
dlabaja <dlabaja.jan@seznam.cz>
2025-09-14 19:07:46 +0200

    lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,13 +3,18 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  tseslint.configs.recommended,
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
-    languageOptions: { globals: globals.browser },
-    rules: {
-      "quotes": ["error", "double"],
-      "@typescript-eslint/no-unused-vars": "warn",
-    }
-  },
+    tseslint.configs.recommended,
+    { 
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+        languageOptions: { globals: globals.browser },
+        rules: {
+            "quotes": ["error", "double"],
+            "indent": ["error", 4, {
+                "SwitchCase": 1
+            }],
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "warn"
+        }
+    },
 ]);
